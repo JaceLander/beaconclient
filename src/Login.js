50d1545704084afce7ca14a5b29cd85e18467c1f
JaceLander <jacethelander@gmail.com>
2025-06-26 22:22:40 -0400

    login(create user) finished

--- src/Login.js
+++ src/Login.js
@@ -4,26 +4,29 @@ import './App.css';
 
 function Login() {
   const [passwordResponse, setPasswordResponse] = useState("");
-  const [passwordText, setPasswordText] = useState("");
+
 
   const handleChange = (e) => {
-    setPasswordText(e.target.value);
-    if(value != null){
-    setPasswordResponse(checkPassword(passwordText).join("\n"));
+    const newPassword = e.target.value;
+    if(newPassword.length === 0){
+    setPasswordResponse('');
+    }else{
+    setPasswordResponse(checkPassword(newPassword));
     }
   }
 
 
   return (
     <div className="App">
-      <header className="Header">Beacon
+      <header className="Header">Beacon.
       </header>
-      <text className='login-item text'>Username</text>
-      <input type='textbox' className='textbox'></input>
-      <text className='login-item text'>Password</text>
-      <input name='passwordText' type='textbox' className='textbox' onChange={handleChange}></input>
-      <text className='login-item text'>{passwordResponse}</text>
-      <button className='login-item button space'>Log In</button>
+      {/* <text className='login-item text'>Username</text> */}
+      <input type='textbox' className='textbox space' placeholder='username'></input>
+      {/* <text className='login-item text'>Password</text> */}
+      <input name='passwordText' type='password' className='textbox pw-space' placeholder= 'password'onChange={handleChange}></input>
+      <div className='response password-space'>{passwordResponse}</div>
+      <button className='login-item button space space-top'>Log In</button>
+      <button className='login-item button space'>Don't have an account?</button>
       <button className='login-item button'>I want to use a trial account</button>
     </div>
   );
--- src/LoginScripts.js
+++ src/LoginScripts.js
@@ -1,27 +1,42 @@
+import './App.css';
 
 
 function checkPassword(passwordString){
     
     const response = [];
+    const responseDivs = [];
+    var valid = false;
 
-
-    if(passwordString.length() < 8){
-        response.push("Password must be longer 8 characters or longer");
+    if(passwordString.length < 8){ 
+        response.push("*Password must be longer 8 characters or longer");
     }
     if(!/[A-Z]/.test(passwordString)){
-        response.push("Password must have at least one uppercase letter");
+        response.push("*Password must have at least one uppercase letter");
     }
     if(!/[a-z]/.test(passwordString)){
-        response.push("Password must have at least one lowercase letter");
+        response.push("*Password must have at least one lowercase letter");
     }
     if(!/[0-9]/.test(passwordString)){
-        response.push("Password must have at least one number");
+        response.push("*Password must have at least one number");
     }
-    if(response.length() === 0)
+    if(response.length === 0)
     {
-        response.push("Password is valid.")
+        response.push("Password is valid!")
+        valid = true;
     }
-    return response;
+        for(let i = 0; i<response.length; i++ )
+        {
+            if(!(valid === true) ){
+            responseDivs.push(<text className='response false'>{response[i]}</text>)
+            }else{
+                responseDivs.push(<text className='response true'>{response[i]}</text>)
+            }
+            
+        }
+    
+
+
+    return responseDivs;
 }
 
 export {checkPassword};
