c31d566dc8755802cd48fb1ef671933a08172938
JaceLander <jacethelander@gmail.com>
2025-06-27 01:05:57 -0400

    ok

---
+++ src/MyProfile.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import {BottomToolbar} from './Toolbar';
+
+
+function MyProfile() {
+
+
+
+
+
+  return (
+    <div className="App">
+        <BottomToolbar></BottomToolbar>
+    </div>
+  );
+
+
+}
+
+export default MyProfile;
---
+++ src/Toolbar.js
@@ -0,0 +1,10 @@
+function BottomToolbar() {
+    return (
+      <div className="bottom-toolbar">
+        <button>Home</button>
+        <button>Search</button>
+        <button>Profile</button>
+      </div>
+    );
+  }
+  export {BottomToolbar};
