685e673899fdc3ab07c0e68b250ac5ff09664d0e
JaceLander <jacethelander@gmail.com>
2025-07-24 00:57:01 -0400

    made front page much prettier

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -2,15 +2,51 @@ import { useState } from 'react';
 import { checkPassword } from './LoginScripts.js';
 import { AnimatePresence, motion } from 'motion/react';
 import './App.css';
+import axios from 'axios';
+import './Login.css';
+import haloBeacon from './halo beacon.png';
 
 function CreateAccount() {
   const [passwordResponse, setPasswordResponse] = useState("");
   const [password2Response, setPassword2Response] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameInput, setUsernameInput] = useState("");
   const [color, setColor] = useState('rgb(0, 0, 0)');
 
+  async function createUser(){
+    const user = {
+      username: usernameInput,
+      passwordHash: password
+    };
+    
+    await fetch("http://localhost:8080/api/user/add", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "1972",
+      },
+    })
+    .then((response) => {
+      if (!response.ok) {
+        // This block is important to catch 4xx or 5xx errors
+        return response.text().then(text => {
+          throw new Error(`HTTP ${response.status}: ${text}`);
+        });
+      }
+      return response.json();
+    })
+    .then((data) => console.log("User created:", data))
+    .catch((error) => console.error("Error creating user:", error.message));
 
-  const handleChange = (e) => {
+  }
+
+  const handleChangeUserName = (e) => {
+    const newUsername = e.target.value;
+    setUsernameInput(newUsername);
+  }
+
+  const handleChangePW = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
     if(newPassword.length === 0){
@@ -20,7 +56,7 @@ function CreateAccount() {
     }
   }
 
-  const handleChangePW = (e) => {
+  const handleChangePW2 = (e) => {
     const EnteredPassword = e.target.value;
     if(EnteredPassword.length === 0){
     setPasswordResponse('');
@@ -37,18 +73,27 @@ function CreateAccount() {
 
 
   return (
-    <div className="App">
+    <div className="Login">
+    <div className='ImageWrapper'> 
+         <img className='background' src={haloBeacon} alt="Halo Beacon"/>
+         <div className="InsetShadow" />
+        </div>
       <header className="Header outfit-title unselectable">Beacon.
       </header>
-      {/* <text className='login-item text'>Username</text> */}
-      <input type='textbox' className='textbox space outfit-text' placeholder='Username'></input>
-      {/* <text className='login-item text'>Password</text> */}
-      <input type='password' className='textbox pw-space outfit-text' placeholder= 'Password'onChange={handleChange}></input>
-      <div className='response password-space outfit-text'>{passwordResponse}</div>
-      <input type='password' className='textbox pw-space outfit-text' placeholder= 'Re-enter Password'onChange={handleChangePW}></input>
-      <div className='response password-space' style={{color: color}}>{password2Response}</div>
-      <button className='login-item button space space-top outfit-text'>Create Account</button>
-    </div>
+      <div className='LoginResponseContainer'>
+          <label className='login-item label'>Create Account</label>
+          <input type='textbox' className='textbox space outfit-text' 
+          placeholder='Username' onChange={handleChangeUserName}></input>
+          <input type='password' className='textbox pw-space outfit-text' placeholder= 
+          'Password'onChange={handleChangePW}></input>
+          <div className='response password-space outfit-text'>{passwordResponse}</div>
+          <input type='password' className='textbox pw-space outfit-text' placeholder= 
+          'Re-enter Password'onChange={handleChangePW2}></input>
+          <div className='response password-space' style={{color: color}}>{password2Response}</div>
+          <button className='login-item button space space-top outfit-text' 
+          onClick={createUser}>Create Account</button>
+       </div>
+       </div>
   );
 
 
--- src/Login.js
+++ src/Login.js
@@ -2,35 +2,96 @@ import { useState } from 'react';
 import { checkPassword, checkUsername} from './LoginScripts.js';
 import './App.css';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import './Login.css';
+import haloBeacon from './halo beacon.png';
 
 function Login() {
   const [passwordResponse, setPasswordResponse] = useState("");
+  const [users, setUsers] = useState ("");
+  const [usernameInput, setUsernameInput] = useState("");
+  const [usernameInputResponse, setUsernameInputResponse] = useState("");
+  const [password, setPassword] = useState("");
+
+  let loginResponse;
+
   const navigate = useNavigate();
 
 
 
-  const handleChange = (e) => {
-    const newPassword = e.target.value;
-    if(newPassword.length === 0){
-    setPasswordResponse('');
-    }else{
-    setPasswordResponse(checkPassword(newPassword));
+async function checkUserPassword(){
+  const login = {
+    username: usernameInput,
+    password: password
+  };
+  
+  fetch("http://localhost:8080/api/user/login", {
+    method: "POST",
+    body: JSON.stringify(login),
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": "1972",
+    },
+  })
+  .then((response) => {
+    if (!response.ok) {
+      // This block is important to catch 4xx or 5xx errors
+      return response.text().then(text => {
+        throw new Error(`HTTP ${response.status}: ${text}`);
+      });
     }
+    loginResponse = response.json();
+
+  })
+  .then((data) => console.log("User sucessfully logged in:", data))
+  .catch((error) => console.error("Error verifying:", error.message));
+
+  alert(loginResponse);
+
+}
+
+
+
+
+  const handlePWChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    if(newPassword.length === 0){
+      setPasswordResponse('');
+    } else 
+    {
+      setPasswordResponse(checkPassword(newPassword));
+    }
+  }
+
+  const handleUNChange = (e) => {
+    const newUsernameInput = e.target.value;
+    setUsernameInput(newUsernameInput)
+  }
+
+  function handleLoginClick(){
+    checkUserPassword();
   }
 
 
   return (
-    <div className="App">
-      <header className="Header outfit-title unselectable">Beacon.
-      </header>
-      <input type='textbox' className='textbox space outfit-text' placeholder='Username'></input>
-      <input name='passwordText' type='password' className='textbox space outfit-text' placeholder= 'Password'onChange={handleChange}></input>
-      <button className='login-item button space outfit-text' >Log In</button>
-      <button className='login-item button space outfit-text small' onClick={() => navigate('/createAccount')}>Don't have an account?</button>
-      <button className='login-item button space  outfit-text small'>I want to use a trial account</button>
-
-    
-    </div>
+      <div className="Login">
+        <div className='ImageWrapper'> 
+         <img className='background' src={haloBeacon} alt="Halo Beacon"/>
+         <div className="InsetShadow" />
+        </div>
+        <header className="Header outfit-title unselectable">Beacon.
+        </header>
+          <div className='LoginResponseContainer'>
+            <label className='login-item label'>Sign in</label>
+            <input type='textbox'  className='textbox space outfit-text' placeholder='Username' onChange={handleUNChange}></input>
+            <input name='textbox' type='password' className='textbox space outfit-text' placeholder= 'Password' onChange={handlePWChange}></input>
+            <button className='buttons login-item button space' style={{backgroundColor:"rgba(255, 255, 255, 1)"}} onClick={checkUserPassword}>Log In</button>
+            <button className='buttons login-item button space' style={{backgroundColor:"rgba(89, 89, 89, .55)"}} onClick={() => navigate('/createAccount')}>Don't have an account?</button>
+            <button className='buttons login-item button space' style={{marginTop:'75px', marginBottom:'0px'}}>Log In As Guest</button>
+          </div>
+          </div>
   );
 
 
